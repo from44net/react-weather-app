2dd5b18b5b76925413634a44cd88babb60ceed1f
from44net <from44net@gmail.com>
2021-07-12 21:13:34 +0900

    first commit

---
+++ src/components/Results.js
@@ -0,0 +1,21 @@
+// Results.js
+
+const Results = (props) => {
+  return(
+    <div>
+      {props.results.cityName && 
+        <div className="results-city">{props.results.cityName}</div>}
+      {props.results.country && 
+        <div className="results-country">{props.results.country}</div>}
+      {props.results.temperature && 
+        <div className="results-temp">{props.results.temperature}</div>}
+      {props.results.conditionText && 
+        <div className="results-condition">
+          <img src={props.results.icon} alt="icon"/>
+          <span>{props.results.conditionText}</span>
+        </div>}
+    </div>
+  );
+}
+
+export default Results;
